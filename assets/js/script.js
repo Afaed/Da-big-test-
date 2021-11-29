8c93f98252e731ca53f8fb3404bc0d293bc3610d
Afaed <44733498+Afaed@users.noreply.github.com>
2021-11-28 18:46:05 -0800

    New commit, tried to fix the error.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,119 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
-var quizstart = document.getElementById('quizstart');
+var quiz = document.getElementById('quizstart');
 var actionContainerEL = document.createElement ("div");
 var currentQuestionIndex = 0;
-var question = 
-quizstart.InnerHtml = function quizstart() {
-    function myquestion() {
-        const myQuestions = [
-        {
-          Questionumber: 1, 
-          question: "Who invented JavaScript?",
-          answers: {
-            a: "Douglas Crockford",
-            b: "Sheryl Sandberg",
-            c: "Brendan Eich",
-            d: "Joe Ket",
-          },
-          correctAnswer: "c"
-        },
-        {
-            Questionnumber: 2,
-            question: "What is an array?",
-            answers: {
-                a: "It contains numberz",
-                b: "It stores data",
-                c: "It contains values",
-                d: "All of the above",
-            },
-            correctAnswer: "d",
-        },
-    ]}}
-
-    function buildQuiz() {
-        const output= [],
-        myquestion(
-            (currentQuesiton, QuestionNumber) => {
-                currentQuesiton = [function quizstart(){myQuestions}]
-                const answers = [];
-                for(letter in myQuestions.answers){}
-            })
-        }
-    
-get.ElementbyId = function countdown() {
-    var timeLeft = 5 ;
-  
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function() {
-      
-      if (timeLeft < 0) {
-        clearInterval(timeInterval) //telss the proram when to end this specific fuction.
-        displayMessage();
-      }else {
-        timerEl.textContent = timeLeft;
-        timeLeft--;
-        }
-    }, 1000); //Function that tells the pc how fast make sure you clear the interval first. as shown above
-    }
-
-function showResults() {
-
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+function Quiz(Questions) {
+    this.score = 0;
+    this.questions = Questions;
+    this.questionIndex = 0
 }
-quizstart (
-    myQuestions(),
-    countdown(),
-)
-//event listeners
 
-quizstart.addeventListener('click', quizstart.InnerHtml)
+var myQuestions = [
+    {
+        question: "Who invented JavaScript?", 
+        a: "Douglas Crockford", 
+        b: "Sheryl Sandberg", 
+        c: "Brendan Eich", 
+        d: "Joe Ket",
+        answer: "D"
+    },
+    {
+        question: "Who invented JavaScript?", 
+        a: "Douglas Crockford", 
+        b: "Sheryl Sandberg", 
+        c: "Brendan Eich", 
+        d: "Joe Ket",
+        answer: "D"
+    },
+    {
+        question: "Who invented JavaScript?", 
+        a: "Douglas Crockford", 
+        b: "Sheryl Sandberg", 
+        c: "Brendan Eich", 
+        d: "Joe Ket",
+        answer: "D"
+    },
+    {
+        question: "Who invented JavaScript?", 
+        a: "Douglas Crockford", 
+        b: "Sheryl Sandberg", 
+        c: "Brendan Eich", 
+        d: "Joe Ket",
+        answer: "D"
+    },
+    {
+        question: "Who invented JavaScript?", 
+        a: "Douglas Crockford", 
+        b: "Sheryl Sandberg", 
+        c: "Brendan Eich", 
+        d: "Joe Ket",
+        answer: "D"
+    },
+        ]
+    function get () {
+    return document.getElementById(quizstart)
+}
+
+function renderQuestion(){
+    test = get("test");
+    if(pos >= questions.length){
+      test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+      get("test_status").innerHTML = "Test completed";
+      // resets the variable to allow users to restart the test
+      pos = 0;
+      correct = 0;
+      // stops rest of renderQuestion function running when test is completed
+      return false;
+    }
+    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    
+    question = questions[pos].question;
+    chA = questions[pos].a;
+    chB = questions[pos].b;
+    chC = questions[pos].c;
+    // display the question
+    test.innerHTML = "<h3>"+question+"</h3>";
+    // display the answer options
+    // the += appends to the data we started on the line above
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+  }
+function checkAnswer(){
+    // use getElementsByName because we have an array which it will loop through
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++){
+      if(choices[i].checked){
+        choice = choices[i].value;
+      }
+    }
+    // checks if answer matches the correct choice
+    if(choice == questions[pos].answer){
+      //each time there is a correct answer this value increases
+      correct++;
+    }
+    // changes position of which character user is on
+    pos++;
+    // then the renderQuestion function runs again to go to next question
+    renderQuestion();
+  }
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+//event listeners
+quizsstart.addEventListner("click", Quiz)
+localStorage.getItem("Initials")
